45364fcbb1e5ef66826c4b2b2846462c1676aba1
Layan Jethwa <lrsjethwa3.14@gmail.com>
2025-09-21 13:08:18 +0100

    build pygbag

---
+++ README.md
@@ -0,0 +1,15 @@
+# Fourier series drawing
+
+This project draws an arbitrary curve using the discrete Fourier series to visualise it as a series of epicycles (rotating circles), functionally a sum of vectors.
+
+## Usage
+
+Click in the left side of the screen to begin the drawing, and click again to finish drawing. As the Fourier series is periodic, a closed loop will work best.
+
+The slower you draw, the more vectors it will have to compute, so don't draw for too long otherwise it will run quite slowly.
+
+The left slider changes the frame rate, which changes the speed of the animation and the sampling rate. The right slider changes the resolution, by reducing or increasing the number of vectors modelled.
+
+## Demo
+
+![Showcase gif]()
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ sys.dont_write_bytecode = True
 import pygame
 import cmath
 import slider
+import asyncio
 pygame.init()
 screen = pygame.display.set_mode((1610, 800))
 pygame.display.set_caption('Fourier drawing')
 running = True
 black = (0,0,0)
 white = (255,255,255)
-import load_svg
 
 
 def lerp(a, b, t, t_min=0, t_max=100):
@@ -74,6 +74,7 @@ update_vectors = False
 draw_colour = (0,255,0)
 
 mode = "draw"
+#import load_svg
 #mode = "plot"
 #points = [complex(i[0], -i[1]) for i in load_svg.load_svg("svg/humberger-svgrepo-com.svg")]
 #points = [complex(i[0], -i[1]) for i in load_svg.load_svg("svg/svg-svgrepo-com.svg", density=0.1, scale=0.5, offset=(-100,-100))]
@@ -81,70 +82,74 @@ mode = "draw"
 #coefficients, full_vectors = compute_fourier(points)
 #vectors = full_vectors
 
+async def main():
+    global running, mode, pen, path, t, drawing, coefficients, vectors, full_vectors, resolution, update_vectors, draw_colour
+    while running:
+        screen.fill(black)
+        pygame.draw.line(screen, white, (805,0), (805,800), 10)
+        speed_slider.draw(screen)
+        resolution_slider.draw(screen)
 
-while running:
-    screen.fill(black)
-    pygame.draw.line(screen, white, (805,0), (805,800), 10)
-    speed_slider.draw(screen)
-    resolution_slider.draw(screen)
+        if mode == "plot":
+            base = (0,0)
+            if update_vectors:
+                draw_colour = (
+                    lerp(255, 0, resolution),
+                    lerp(0, 255, resolution),
+                    0)
+                vectors = sorted(full_vectors, key=lambda v: abs(v.polar[0]), 
+                                        reverse=True)[:int(len(full_vectors)*resolution/100)]
+                path = []
+                update_vectors = False
+            for vector in vectors:
+                vector.update(t)
+                base = draw_vector(screen, base, vector)
 
-    if mode == "plot":
-        base = (0,0)
-        if update_vectors:
-            draw_colour = (
-                lerp(255, 0, resolution),
-                lerp(0, 255, resolution),
-                0)
-            vectors = sorted(full_vectors, key=lambda v: abs(v.polar[0]), 
-                                     reverse=True)[:int(len(full_vectors)*resolution/100)]
-            path = []
-            update_vectors = False
-        for vector in vectors:
-            vector.update(t)
-            base = draw_vector(screen, base, vector)
+            path.append(coordinate(*base))
+            if len(path) > 2:
+                pygame.draw.lines(screen, draw_colour, False, path, 1)
+            
+            t += 1
 
-        path.append(coordinate(*base))
-        if len(path) > 2:
-            pygame.draw.lines(screen, draw_colour, False, path, 1)
-        
-        t += 1
+        elif mode == "draw" and pen:
+            drawing.append(pygame.mouse.get_pos())
+        if len(drawing) > 2:
+            pygame.draw.lines(screen, white, False, drawing, 1)
 
-    elif mode == "draw" and pen:
-        drawing.append(pygame.mouse.get_pos())
-    if len(drawing) > 2:
-        pygame.draw.lines(screen, white, False, drawing, 1)
+        for event in pygame.event.get():
+            speed_slider.handle_event(event)
+            resolution_slider.handle_event(event)
+            if resolution_slider.value != resolution:
+                resolution = resolution_slider.value
+                update_vectors = True
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+                exit()
+                quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if not pen and pygame.mouse.get_pos()[0] <= 800:
+                        mode = "draw"
+                        pen = True
+                        drawing = []
+                        coefficients = []
+                        full_vectors = []
+                        vectors = []
+                        path = []
+                    elif pygame.mouse.get_pos()[0] <= 800:
+                        pen = False
+                        mode = "plot"
+                        points = [complex(*(coordinate(*i, mode="draw"))) for i in drawing]
+                        coefficients, full_vectors = compute_fourier(points)
+                        vectors = sorted(full_vectors, key=lambda v: abs(v.polar[0]), 
+                                        reverse=True)[:int(len(full_vectors)*resolution/100)]
 
-    for event in pygame.event.get():
-        speed_slider.handle_event(event)
-        resolution_slider.handle_event(event)
-        if resolution_slider.value != resolution:
-            resolution = resolution_slider.value
-            update_vectors = True
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-            sys.exit()
-            exit()
-            quit()
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                if not pen and pygame.mouse.get_pos()[0] <= 800:
-                    mode = "draw"
-                    pen = True
-                    drawing = []
-                    coefficients = []
-                    full_vectors = []
-                    vectors = []
-                    path = []
-                elif pygame.mouse.get_pos()[0] <= 800:
-                    pen = False
-                    mode = "plot"
-                    points = [complex(*(coordinate(*i, mode="draw"))) for i in drawing]
-                    coefficients, full_vectors = compute_fourier(points)
-                    vectors = sorted(full_vectors, key=lambda v: abs(v.polar[0]), 
-                                     reverse=True)[:int(len(full_vectors)*resolution/100)]
+        pygame.display.update()
+        clock.tick(speed_slider.value)
+        await asyncio.sleep(0)
 
-    pygame.display.update()
-    clock.tick(speed_slider.value)
+asyncio.run(main())
 
     
